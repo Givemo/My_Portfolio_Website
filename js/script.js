4444cbc5f2a92b3e33621099af95f84415a0aa5a
givemo <gtchiwanza@gmail.com>
2020-08-08 18:12:25 +0100

    Added dark theme feature and testimonial

--- js/script.js
+++ js/script.js
@@ -1,83 +1,81 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav__link");
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
 });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
-    })
-})
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
 
-const TypeWriter = function(txtElement, words, wait = 2000) {
-    this.txtElement = txtElement;
-    this.words = words;
-    this.txt = '';
-    this.wordIndex = 0;
-    this.wait = parseInt(wait, 10);
-    this.type();
-    this.isDeleting = false;
-}
+const TypeWriter = function (txtElement, words, wait = 2000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
 
 //Type Method
-TypeWriter.prototype.type = function() {
-    //Current index of word 
-    const current = this.wordIndex % this.words.length;
-    //Get full text of current word
-    const fullTxt = this.words[current];
-    
-    //Check if deleting
-    if(this.isDeleting) {
-        //Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        //Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+TypeWriter.prototype.type = function () {
+  //Current index of word
+  const current = this.wordIndex % this.words.length;
+  //Get full text of current word
+  const fullTxt = this.words[current];
 
-    //Insert txt into element
-    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+  //Check if deleting
+  if (this.isDeleting) {
+    //Remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    //Add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
 
-    //Initial Type Speed
-    let typeSpeed = 300
+  //Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
-    if(this.isDeleting) {
-        typeSpeed /= 2;
-    }
+  //Initial Type Speed
+  let typeSpeed = 300;
 
-        //If word is complete
-    if(!this.isDeleting && this.txt === fullTxt) {
-        //Make pause at end
-        typeSpeed = this.wait;
-        //Set delete to true
-        this.isDeleting = true;
-    } else if(this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        //Move to the next word
-        this.wordIndex++;
-        //Pause before start typing
-        typeSpeed = 500
-    }
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
 
+  //If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    //Make pause at end
+    typeSpeed = this.wait;
+    //Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    //Move to the next word
+    this.wordIndex++;
+    //Pause before start typing
+    typeSpeed = 500;
+  }
 
-    setTimeout(() => this.type(), typeSpeed);
-}
+  setTimeout(() => this.type(), typeSpeed);
+};
 
 //Initialise on DOM Load
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 //Init App
 function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
 
-    //Init typeWriter
-    new TypeWriter(txtElement, words, wait)
+  //Init typeWriter
+  new TypeWriter(txtElement, words, wait);
 }
-
 
 /* //ES6 Class instead from Type method 
 class TypeWriter {
@@ -150,3 +148,33 @@ function init() {
 }
  */
 
+/* Toogle theme*/
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem("theme", themeName);
+  document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-light");
+    document.body.style.background = "var(--clr-light)";
+    document.body.style.color = "var(--clr-dark)";
+  } else {
+    setTheme("theme-dark");
+    document.body.style.background = "var(--clr-dark)";
+    document.body.style.color = "var(--clr-light)";
+  }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem("theme") === "theme-dark") {
+    setTheme("theme-dark");
+    document.getElementById("slider").checked = false;
+  } else {
+    setTheme("theme-light");
+    document.getElementById("slider").checked = true;
+  }
+})();
